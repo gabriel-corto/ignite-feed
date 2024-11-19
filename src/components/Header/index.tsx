dec734d182c34dd0726b986634304e9cfc9e8f0f
gabriel-corto <digital.gabriel2006@gmail.com>
2024-11-19 09:15:35 +0100

    fix: add resonsible app layout

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -10,42 +10,6 @@ export function Header() {
         <img src={logoIgnite} alt="" />
         Ignite Feed
       </strong>
-
-      <nav className={styles.navbar}>
-        <ul>
-          <li>
-            <a href="#">
-              <House weight="bold" />
-              <span>Home</span>
-            </a>
-          </li>
-          <li>
-            <a href="#">
-              <MagnifyingGlass weight="bold" />
-              <span>Buscar artigos</span>
-            </a>
-          </li>
-          <li>
-            <a href="#">
-              <Bell weight="bold" />
-              <span>Notificações</span>
-            </a>
-          </li>
-          <li>
-            <a href="#">
-              <User weight="bold" />
-              <span>Perfil</span>
-            </a>
-          </li>
-        </ul>
-      </nav>
-
-      <div>
-        <button>
-          <PencilCircle weight="bold" />
-          <span>Publicar um Post</span>
-        </button>
-      </div>
     </header>
   )
 }
